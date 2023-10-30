90d615a034d3fd100b209d5fc5f6305a1acd55cd
Calebbuffleben <calebbuffleben@gmail.com>
2023-10-30 19:20:27 -0300

    User is Admin

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,8 +3,10 @@ import { Controller, Post } from '@nestjs/common';
 @Controller('users')
 export class UsersController {
   @Post()
-  async addUser(@Body() user:User) {
-    addUser(user);
+  async addUser(@Body() user: User) {
+    if(user.role === 'Admin'){
+        addUser(user);
+    }
 
     return { message: 'User Added'}
   }
