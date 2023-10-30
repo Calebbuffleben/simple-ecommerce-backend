bb92d950070341f11304f4e8bf8806d1bc33635c
Calebbuffleben <calebbuffleben@gmail.com>
2023-10-30 19:27:24 -0300

    User arleady exists

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,9 @@
+import { BadRequestException } from '@nestjs/common';
+
 export class UsersService {
-  async createUser(createUserDTO: CreateUserDTO){
-    
+  async createUser(createUserDTO: CreateUserDTO) {
+    if (createUserDTO.email) {
+      throw new BadRequestException('The users arleady exists');
+    }
   }
 }
