1c2782a06ce6123346f8aeacc400d13dc5a1bfae
Calebbuffleben <calebbuffleben@gmail.com>
2023-10-31 08:29:07 -0300

    user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,12 @@ import { BadRequestException } from '@nestjs/common';
 
 export class UsersService {
   async createUser(createUserDTO: CreateUserDTO) {
-    if (createUserDTO.email) {
+    if (createUserDTO.email && isValidCpf(createUserDTO.cpf)) {
       throw new BadRequestException('The users arleady exists');
     }
+
+    const user = new this.userModel(createUserDTO);
+    await user.save();
+    return user;
   }
 }
