d6d3afb6220f21f2f8c58b60f17ce97fe7f253fe
Calebbuffleben <calebbuffleben@gmail.com>
2023-10-31 08:50:35 -0300

    find users by email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,12 +3,19 @@ import { isValidCpf } from 'src/helpers/validate-cpf.helper';
 
 export class UsersService {
   async createUser(createUserDTO: CreateUserDTO) {
-    if (createUserDTO.email && isValidCpf(createUserDTO.cpf)) {
+    if (
+      this.findUserByEmail(createUserDTO.email) &&
+      !isValidCpf(createUserDTO.cpf)
+    ) {
       throw new BadRequestException('The users arleady exists');
     }
 
     const user = new this.userModel(createUserDTO);
     await user.save();
     return user;
+  }
+
+  async findUserByEmail(email) {
+    this.userModel.findOne({ email });
   }
 }
