0e27ccc3bc2acd2387e6b984cb4009c99f45d2d5
Calebbuffleben <calebbuffleben@gmail.com>
2024-12-10 11:36:53 -0300

    fix: change name paymentRouts.ts

--- src/infrastructure/routes/paymentRoutes.ts
+++ src/infrastructure/routes/paymentRoutes.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import { PaymentController } from '../../adapters/controllers/PaymentController';
 
 const router = express.Router();
-const controller  = new PaymentController();
+const paymentController  = new PaymentController();
 
 router.post('/pay', async (req, res) => {
-    await controller.handlePayment(req, res);
+    await paymentController.handlePayment(req, res);
 });
 
 export default router;
